247acd04304db0d5bc78e7f7d598761343fedeaf
TravisCoutrue <travis.m.couture@gmail.com>
2023-02-12 19:34:16 -0700

    Video cards to be rendered into UI library.

---
+++ src/VideoCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function VideoCard({ videoData }) {
+    return (
+        <div className="col">
+            <div className="card shadow mb-2">
+                <video className="card-img-top" src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" controls preload="metadata" playsInline/>
+                <div className="card-body">
+                    <h5 className="card-title">{ videoData.metadata.filename }</h5>
+                    <p className="card-text">{ videoData.metadata.duration }</p>
+                    <button className="btn btn-primary">Select</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default VideoCard;
