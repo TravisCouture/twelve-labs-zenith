f5df0471aab34b543c688aceaf199b51c705f82c
TravisCoutrue <travis.m.couture@gmail.com>
2023-02-14 14:41:59 -0700

    Added call for video-to-video search.

--- src/Api.js
+++ src/Api.js
@@ -11,3 +11,29 @@ export async function getVideos (api_url, index_id, api_key) {
     const response = await fetch(videos_url, options);
     return await response.json();
 };
+
+export async function getSimilarVideos(api_url, api_key, index_id, video_id, start, end) {
+    const v2v_search_url = `${api_url}search/video`;
+
+    const options = {
+        method: "POST",
+        headers: {
+            "x-api-key": api_key,
+            accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "index_id": index_id,
+            "page_limit": 50,
+            "query": {
+                "video_id": video_id,
+                "start": start,
+                "end": end
+            }
+        })
+    };
+
+    console.log("Sending POST request");
+    const response = await fetch(v2v_search_url, options);
+    console.log(await response.then((json) => {return json}));
+};
