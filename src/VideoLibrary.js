5e5ef1f89fe39b9459ddcdfd91b4af512a800255
TravisCoutrue <travis.m.couture@gmail.com>
2023-02-13 18:36:27 -0700

    Migrated to functional component.

--- src/VideoLibrary.js
+++ src/VideoLibrary.js
@@ -7,57 +7,46 @@ const API_URL = process.env.REACT_APP_TWELVE_LABS_API_URL;
 const API_KEY = process.env.REACT_APP_TWELVE_LABS_API_KEY;
 const VIDEO_TO_VIDEO_INDEX = process.env.REACT_APP_TWELVE_LABS_VIDEO_VIDEO_INDEX;
 
-const videoData = {
-    url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-    title: "For Bigger Blazes",
-    description: "This is a test description. The dragons are breathing fire."
-  };
+function VideoLibrary() {
+    const [focusVideoState, setFocusVideoState] = React.useState({
+        focusVideoStart: 0,
+        focusVideoEnd: 0,
+        startThumb: 0,
+        endThumb: 0
+    });
 
-class VideoLibrary extends React.Component {
-    constructor( props ) {
-        super( props );
-        this.focusVideo = React.createRef();
-        this.selectedVideo = React.createRef();
+    const [focusVideo, setFocusVideo] = React.useState();
 
-        this.state = {
-            videos: null,
-            focusVideo: null
-        };
-    };
+    const [indexVideos, setIndexVideos] = React.useState();
 
-    componentDidMount() {
+    React.useEffect(() => {
         const response = getVideos(API_URL, VIDEO_TO_VIDEO_INDEX, API_KEY);
-        response.then((json) => this.setState({
-                videos: json.data,
-                focusVideo: json.data[0]
-            }
-        ));
-    };
+        response.then((json) => {
+            setIndexVideos(json.data);
+            setFocusVideo(json.data[0]);
+        });
+    }, []);
 
-    componentDidUpdate() {
-    };
+    if (indexVideos) {
+        return (
+            <div className="VideoLibrary">
+                <div className="container-fluid">
+                    <div className="row row-cols-2 justify-content-center scroll-row">
 
-    render () {
-        if (this.state.videos !== null) {
-            return (
-                <div className="VideoLibrary">
-                    <div className="container-fluid">
-                        <div className="row row-cols-2 justify-content-center scroll-row">
-                            <div className="col-5 justify-content-center ">
-                                <FocusVideoCard videoData={ this.state.focusVideo } />
-                            </div>
-                            
-                            <div className="col scroll-col">
-                                <div className="row row-cols-2">
-                                    {this.state.videos.map(video => <VideoCard videoData={ video }/>)}
-                                </div>
-                            </div>
-
+                        <div className="col-4">
+                            <FocusVideoCard focusVideoState={ focusVideoState } setFocusVideoState={ setFocusVideoState } videoData={ focusVideo }/>
                         </div>
+                        
+                        <div className="col scroll-col">
+                            <div className="row row-cols-3">
+                                { indexVideos.map((video, index) => <VideoCard setFocusVideo={ setFocusVideo } videoData={ video } key={ index } />) }
+                            </div>
+                        </div>
+
                     </div>
                 </div>
-            );
-        };
+            </div>
+        );
     };
 };
 
