98639aa2d654d11dfa381a21911f81795ad6d4e3
TravisCoutrue <travis.m.couture@gmail.com>
2023-02-14 22:14:40 -0700

    Can now handle search results.

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -2,26 +2,65 @@ import React from "react";
 import ReactPlayer from "react-player/youtube";
 
 function VideoCard({ setFocusVideo, videoData, url }) {
+    let videoCard;
+
+    if (videoData.start) {
+        videoCard = <div className="card shadow mb-3 flex-fill">
+                        <ReactPlayer 
+                            className="card-img-top card-video-top embed-responsive"
+                            url={ `${url}?start=${videoData.start}&end=${videoData.end}` }
+                            muted={ true }
+                            controls={ true }
+                            playsinline={ true }
+                            height="auto"
+                            width="auto"
+                            light={ true }
+                            config={
+                                { 
+                                    youtube: 
+                                    { playerVars: 
+                                        { 
+                                            origin: 'http://localhost:3000/', 
+                                            enablejsapi: 1
+                                        } 
+                                } 
+                            }}
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">{ videoData.metadata.filename.split("-")[0] }</h5>
+                            <p className="card-text">
+                                { `${Math.round(videoData.metadata.duration / 60)} minutes` }<br/>
+                            </p>
+                            <button className="btn btn-primary" onClick={ () => setFocusVideo(videoData)}>Select</button><br/>
+                            <span className="badge bg-primary mt-2 p-2">Confidence: { videoData.confidence }</span>
+                            <span className="badge bg-primary mx-1 p-2">Start: { Math.round(videoData.start) }s</span>
+                            <span className="badge bg-primary mt-1 p-2">End: { Math.round(videoData.end) }s</span>
+                        </div>
+                    </div>
+    } else {
+        videoCard = <div className="card shadow mb-3 flex-fill">
+                        <ReactPlayer 
+                            className="card-img-top card-video-top embed-responsive"
+                            url={ url }
+                            muted={ true }
+                            controls={ true }
+                            playsinline={ true }
+                            height="auto"
+                            width="auto"
+                            light={ true }
+                            config={{ youtube: { playerVars: { origin: 'http://localhost:3000/', enablejsapi: 1 } } }}
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">{ videoData.metadata.filename.split("-")[0] }</h5>
+                            <p className="card-text">{ `${Math.round(videoData.metadata.duration / 60)} minutes` }</p>
+                            <button className="btn btn-primary" onClick={ () => setFocusVideo(videoData)}>Select</button>
+                        </div>
+                    </div>
+    };
+
     return (
         <div className="col-auto d-flex">
-            <div className="card shadow mb-3 flex-fill">
-                <ReactPlayer 
-                    className="card-img-top card-video-top embed-responsive"
-                    url={ url }
-                    muted={ true }
-                    controls={ true }
-                    playsinline={ true }
-                    height="auto"
-                    width="auto"
-                    light={ true }
-                    config={{ youtube: { playerVars: { origin: 'http://localhost:3000/', enablejsapi: 1 } } }}
-                />
-                <div className="card-body">
-                    <h5 className="card-title">{ `${videoData.metadata.filename.split("-")[0]}` }</h5>
-                    <p className="card-text">{ `${Math.round(videoData.metadata.duration / 60)} minutes` }</p>
-                    <button className="btn btn-primary" onClick={ () => setFocusVideo(videoData)}>Select</button>
-                </div>
-            </div>
+           { videoCard }
         </div>
     );
 }
