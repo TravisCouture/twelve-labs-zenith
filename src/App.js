d5831274fdcee4690a7cc33b1b6317ec1b96ac9f
TravisCoutrue <travis.m.couture@gmail.com>
2023-02-14 22:14:23 -0700

    Better rendering.

--- src/App.js
+++ src/App.js
@@ -12,14 +12,24 @@ const videoData = {
 function App() {
   return (
     <div className="App">
-      <nav className="navbar sticky-top mb-2">
-        <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid mb-3">
           <a className="navbar-brand" href="/">
-            <img src={ LOGO } alt="Logo" className="d-inline-block align-text-top" />
+            <img src={ LOGO } alt="Logo" className="d-inline-block align-text-top" width="300px"/>
           </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Video-To-Video Search</a>
+            </li>
+          </ul>
         </div>
-      </nav>
-      <VideoLibrary />
+      </div>
+    </nav>
+    <VideoLibrary />
     </div>
   );
 }
