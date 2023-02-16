6fc2aafb293a23dbb04b6cc867a7a53e8cf8f2a6
TravisCoutrue <travis.m.couture@gmail.com>
2023-02-16 11:54:08 -0700

    Fixed routing for deployment with GitHub pages.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import './App.css';
 
 const LOGO = "https://twelvelabs.io/logo.png";
 
-const videoData = {
-  url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-  title: "For Bigger Blazes",
-  description: "This is a test description. The dragons are breathing fire."
-};
-
 function App() {
   return (
     <div className="App">
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid mb-3">
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" href="/twelve-labs-zenith">
             <img src={ LOGO } alt="Logo" className="d-inline-block align-text-top" width="300px"/>
           </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,7 +17,7 @@ function App() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Video-To-Video Search</a>
+              <a className="nav-link active" aria-current="page" href="/twelve-labs-zenith">Video-To-Video Search</a>
             </li>
           </ul>
         </div>
